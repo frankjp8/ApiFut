b040cc58da426cad37ab33f44ff9b3590c23a182
frankjp8 <franklynjp8@gmail.com>
2022-02-28 21:57:07 -0400

    tablas

---
+++ APIFut/Controllers/TablasController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using APIFut.Models;
+using Newtonsoft.Json;
+
+namespace APIFut.Controllers
+{
+    [ApiController]
+    [Route("api/tablas/[action]")]
+    public class TablasController : Controller
+    {
+        readonly string url = "https://api.football-data.org/v2/";
+        Root tabla = new Root();
+        [HttpGet]
+        public async Task<Root> LaLiga()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Add("X-Auth-Token", "d4412d78f58e49e597864a451a38bc6e");
+               
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await client.GetAsync("competitions/PD/standings");
+                if (res.IsSuccessStatusCode)
+                {
+                    var response = res.Content.ReadAsStringAsync().Result;
+                    tabla = JsonConvert.DeserializeObject<Root>(response);
+                }
+                return tabla;
+            }
+        }
+        [HttpGet]
+        public async Task<Root> Premier()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Add("X-Auth-Token", "d4412d78f58e49e597864a451a38bc6e");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await client.GetAsync("competitions/PL/standings");
+                if (res.IsSuccessStatusCode)
+                {
+                    var response = res.Content.ReadAsStringAsync().Result;
+                    tabla = JsonConvert.DeserializeObject<Root>(response);
+                }
+                return tabla;
+            }
+        }
+        [HttpGet]
+        public async Task<Root> Bundes()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Add("X-Auth-Token", "d4412d78f58e49e597864a451a38bc6e");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await client.GetAsync("competitions/BL1/standings");
+                if (res.IsSuccessStatusCode)
+                {
+                    var response = res.Content.ReadAsStringAsync().Result;
+                    tabla = JsonConvert.DeserializeObject<Root>(response);
+                }
+                return tabla;
+            }
+        }
+        [HttpGet]
+        public async Task<Root> Ligue1()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Add("X-Auth-Token", "d4412d78f58e49e597864a451a38bc6e");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await client.GetAsync("competitions/FL1/standings");
+                if (res.IsSuccessStatusCode)
+                {
+                    var response = res.Content.ReadAsStringAsync().Result;
+                    tabla = JsonConvert.DeserializeObject<Root>(response);
+                }
+                return tabla;
+            }
+        }
+        [HttpGet]
+        public async Task<Root> SerieA()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Add("X-Auth-Token", "d4412d78f58e49e597864a451a38bc6e");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage res = await client.GetAsync("competitions/SA/standings");
+                if (res.IsSuccessStatusCode)
+                {
+                    var response = res.Content.ReadAsStringAsync().Result;
+                    tabla = JsonConvert.DeserializeObject<Root>(response);
+                }
+                return tabla;
+            }
+        }
+    }
+}
---
+++ APIFut/Startup.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIFut
+{
+    public class Startup
+    {
+        private readonly string _MyCors = string.Empty;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: _MyCors, builder =>
+                {
+                    builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost")
+                     .AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors(_MyCors);
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
